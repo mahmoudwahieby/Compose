71baacbeacadd84bad8402ead7e241080929366a
Mahmoud Wahieby <47131083+mahmoudwahieby@users.noreply.github.com>
2024-04-26 11:02:26 +0300

    update

---
+++ app/src/main/java/com/my/compose/Summary.kt
@@ -0,0 +1,22 @@
+package com.my.compose
+
+// # Summary 001
+// - Describe what, not how
+// - Ul elements are functions
+// - State controls Ul
+// - Events control State
+
+// # Summary 002
+// - Create composable using the @Composable annotation
+// - It's quick & easy to create composable
+// - Composable accept parameters
+// - Use MutableState and remember
+// - Composable should be side-effect free
+
+// # Summary 003
+// # Composable can :
+// - Execute in any order
+// - Run in parallel
+// - Be skipped!
+// - Run frequently
+
